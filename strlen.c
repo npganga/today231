068edf43b4a7a9173544bfecd71c4b6069ef2467
npganga <gangangpalayam@gmail.com>
2016-12-23 15:30:40 +0530

    Add files via upload

---
+++ strlen.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char str[10];
+int len;
+printf("\n enter the string");
+scanf("%s",&str);
+len=strlen(str);
+printf("\n the length of string is%d",len);
+}
